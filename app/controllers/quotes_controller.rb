e962ca5e0e4a52c746c645af77d22e977dee6e2a
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-01 13:50:35 +0000

    db:migrate Quote model creation, Create Quotes controller, add routes to Quote resource, add Quote controller actions

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,42 @@
+class QuotesController < ApplicationController
+  before_action set_quote, only: [:show, :edit, :update, :destroy]
+  def index
+    @quotes = Quote.all
+  end
+  def show
+  end
+  def new
+    @quote = Quote.new
+  end
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      redirect_to quote_path(@quote), status: :see_other, notice: "Quote was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+  end
+  def update
+    if @quote.save
+      redirect_to quote_path(@quote), status: :see_other, notice: "Quote was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, status: :see_other, notice: "Quote was successfully destroyed."
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
+end
