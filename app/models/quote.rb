349d2b493c2225771667f112f02284ad10c07617
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-14 22:26:49 +0000

    Real-time CRUD on the Quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,5 +3,7 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
