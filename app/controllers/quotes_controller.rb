8e9196895d9d755965dcf8416add311ae3992978
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-19 16:57:09 +0000

    Flash notice fixed for create action, turbo

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,7 +13,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, status: :see_other, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was succesfully." }
       end
     else
       render :new, status: :unprocessable_entity
